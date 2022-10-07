c6cd681c5e6382ce78eea760afdad8e2536ab1c5
Lerpardo <ls6182315@gmail.com>
2022-10-07 16:57:41 -0300

    #1 Requisito
    Co-authored-by: Lucas Dias <LucasDiasAl@users.noreply.github.com>
    Co-authored-by: Leonardo Luniere <LeonardoLuniere@users.noreply.github.com>
    Co-authored-by: Marco Viana <MarcoViana0303@users.noreply.github.com>

---
+++ src/Redux/reducers/loginReducer.js
@@ -0,0 +1,20 @@
+import { USER_LOGIN } from  '../actions'
+
+
+const USER_INICIAL = {
+    email: '',
+    name: '',
+}
+
+const user = (state = USER_INICIAL, { payload, type }) => {
+    switch (type) {
+        case USER_LOGIN:
+            return {
+                ...state,...payload,
+            };
+        default:
+            return state
+    }
+}
+
+export default user;
