9f673e4de1fedb3b5f6d9d03816082e06b152446
Lucas Dias <lucaalencarID@gmail.com>
2022-10-07 17:11:18 -0300

    requisito 1

--- src/Redux/actions/index.js
+++
@@ -1,3 +0,0 @@
-export const USER_LOGIN = 'USER_LOGIN';
-
-export const userLogin = (payload) => ({ type: USER_LOGIN, payload, });
--- src/Redux/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import user from './loginReducer';
-
-const rootReducer = combineReducers({
- user,
-});
-
-
-export default rootReducer;
