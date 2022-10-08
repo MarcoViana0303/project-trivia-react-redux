d2a772672c337c09a4cabb6974851422addec1d5
Marco Viana <marcoviana100@gmail.com>
2022-10-07 21:39:49 -0300

    Req 05 parcialmente feito

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import Header from '../components/Header';
 
 class Game extends Component {
   render() {
     return (
-      <>oi</>
+      <div>
+        <Header />
+      </div>
+
     );
   }
 }
